d75ffa04e7f237ad3b3201997966c9ee0494b55d
dvsenna64 <dvsenna64@gmail.com>
2024-04-29 16:01:59 +0200

    Add some documentation

--- lib/include/se/duration.hpp
+++ lib/include/se/duration.hpp
@@ -8,6 +8,11 @@
 
 
 namespace se {
+	/**
+	 * @brief Helper to store, convert and use time with different units
+	 * @tparam T The type to use to store the time
+	 * @tparam Ratio A `se::concepts::Ratio`-complient type
+	 */
 	template <typename T, typename Ratio = se::Ratio<1, 1>>
 	SE_REQUIRES(se::concepts::Number<T> && se::concepts::Ratio<Ratio>)
 	class Duration final {
@@ -15,23 +20,48 @@ namespace se {
 			inline Duration();
 			~Duration() = default;
 
+			/**
+			 * @brief Initialized the duration with the value of `val`
+			 * @param val The value of the duration, directly in the units you want
+			 */
 			inline explicit Duration(T val);
 
+			/**
+			 * @brief Conversion constructor
+			 */
 			template <typename T2, typename Ratio2>
 			inline Duration(const se::Duration<T2, Ratio2> &duration);
+			/**
+			 * @brief Conversion operator
+			 */
 			template <typename T2, typename Ratio2>
 			inline const se::Duration<T, Ratio> &operator=(const se::Duration<T2, Ratio2> &duration);
 
+			/**
+			 * @brief Add a duration to the current one (implicit cast)
+			 */
 			template <typename T2, typename Ratio2>
 			inline const se::Duration<T, Ratio> &operator+=(const se::Duration<T2, Ratio2> &duration);
+			/**
+			 * @brief Substract a duration from the current one (implicit cast)
+			 */
 			template <typename T2, typename Ratio2>
 			inline const se::Duration<T, Ratio> &operator-=(const se::Duration<T2, Ratio2> &duration);
 
+			/**
+			 * @brief Add two durations (implicit cast)
+			 */
 			template <typename T2, typename Ratio2>
 			inline se::Duration<T, Ratio> operator+(const se::Duration<T2, Ratio2> &duration);
+			/**
+			 * @brief Substract two durations (implicit cast)
+			 */
 			template <typename T2, typename Ratio2>
 			inline se::Duration<T, Ratio> operator-(const se::Duration<T2, Ratio2> &duration);
 
+			/**
+			 * @brief Get the value of the duration, in the duration's unit
+			 */
 			inline explicit operator T() const noexcept;
 
 		private:
