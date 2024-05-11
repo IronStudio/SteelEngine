2704e5bc63aea3559a894ba5752d0d5e3aa6e9e9
dvsenna64 <dvsenna64@gmail.com>
2024-05-11 22:29:54 +0200

    Working on window

---
+++ lib/include/se/window/window.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <string>
+
+#ifdef SE_WSI_SDL3
+	#include <SDL3/SDL.h>
+#endif
+
+#include "se/concepts.hpp"
+#include "se/core.hpp"
+#include "se/renderer/graphicsApi.hpp"
+#include "se/types.hpp"
+
+
+
+namespace se::window {
+	struct WindowInfos {
+		se::Int width;
+		se::Int height;
+		std::string title;
+		se::renderer::GraphicsApi graphicsApi;
+	};
+}
+
+
+#ifndef SE_APPLE
+namespace se::concepts {
+	template <typename T>
+	concept WindowImplementation = requires(T obj) {
+		T();
+		{obj.create(se::window::WindowInfos())} -> std::same_as<void>;
+		{obj.destroy()} -> std::same_as<void>;
+		{obj.move(T())} -> std::same_as<void>;
+	};
+}
+#endif
+
+
+namespace se::window
+{
+	template <typename Implementation>
+	SE_REQUIRES(se::concepts::WindowImplementation<Implementation>)
+	class WindowBase {
+		public:
+			inline WindowBase();
+			inline ~WindowBase();
+
+			inline WindowBase(const se::window::WindowInfos &infos);
+
+			inline WindowBase(const se::window::WindowBase<Implementation> &) = delete;
+			inline const se::window::WindowBase<Implementation> &operator=(const se::window::WindowBase<Implementation> &) = delete;
+
+			inline WindowBase(se::window::WindowBase<Implementation> &&window) noexcept;
+			inline const se::window::WindowBase<Implementation> &operator=(se::window::WindowBase<Implementation> &&window) noexcept;
+
+
+		private:
+			Implementation m_implementation;
+	};
+
+
+
+#ifdef SE_WSI_SDL3
+
+	class SE_CORE SDL3Window {
+		public:
+			SDL3Window();
+			~SDL3Window();
+
+			void create(const se::window::WindowInfos &infos);
+			void destroy();
+			void move(SDL3Window &&window);
+
+		private:
+			SDL_Window *m_window;
+	};
+
+
+	using Window = WindowBase<SDL3Window>;
+
+#endif
+
+} // namespace se::window
+
+
+#include "se/window/window.inl"
--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -16,6 +16,7 @@
 #include <se/ecs/scene.hpp>
 #include <se/utils/version.hpp>
 #include <se/renderer/vulkan/context.hpp>
+#include <se/window/window.hpp>
 
 
 using namespace se::literals;
@@ -49,6 +50,30 @@ class SandboxApp : public se::Application {
 			contextInfos.applicationVersion = "1.0.0"_v;
 			contextInfos.preferredGPU = se::renderer::GPUType::eDiscret;
 			se::renderer::vulkan::Context context {contextInfos};
+
+
+			SDL_Init(SDL_INIT_VIDEO);
+			{
+				se::window::WindowInfos windowInfos {};
+				windowInfos.graphicsApi = se::renderer::GraphicsApi::eVulkan;
+				windowInfos.title = "SteelEngine_sandbox";
+				windowInfos.width = 16 * 70;
+				windowInfos.height = 9 * 70;
+				se::window::Window window {windowInfos};
+
+				bool running {true};
+				while (running) {
+					SDL_Event event {};
+					while (SDL_PollEvent(&event)) {
+						if (event.type == SDL_EVENT_QUIT) {
+							running = false;
+							break;
+						}
+					}
+				}
+			}
+
+			SDL_Quit();
 		}
 };
 
