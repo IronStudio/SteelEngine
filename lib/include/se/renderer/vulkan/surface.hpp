d891e234217d3058a833bc9079f587f53c79c48d
Dv-Senna <dvsenna64@gmail.com>
2024-05-22 16:59:02 +0200

    Working on swapchain

--- lib/include/se/renderer/vulkan/surface.hpp
+++ lib/include/se/renderer/vulkan/surface.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include <vulkan/vulkan.h>
 
 #include "se/window/window.hpp"
@@ -17,11 +19,16 @@ namespace se::renderer::vulkan {
 			Surface(const se::renderer::vulkan::SurfaceInfos &infos);
 			~Surface();
 
+			void queryInformations(VkPhysicalDevice physicalDevice);
+
 			inline VkSurfaceKHR getSurface() const noexcept {return m_surface;}
 
 		private:
 			se::renderer::vulkan::SurfaceInfos m_infos;
 			VkSurfaceKHR m_surface;
+			VkSurfaceCapabilitiesKHR m_surfaceCapabilities;
+			std::vector<VkSurfaceFormatKHR> m_surfaceFormats;
+			std::vector<VkPresentModeKHR> m_presentModes;
 	};
 
 } // namespace se::renderer::vulkan
--- lib/src/renderer/vulkan/surface.cpp
+++ lib/src/renderer/vulkan/surface.cpp
@@ -10,7 +10,10 @@
 namespace se::renderer::vulkan {
 	Surface::Surface(const se::renderer::vulkan::SurfaceInfos &infos) :
 		m_infos {infos},
-		m_surface {VK_NULL_HANDLE}
+		m_surface {VK_NULL_HANDLE},
+		m_surfaceCapabilities {},
+		m_surfaceFormats {},
+		m_presentModes {}
 	{
 		if (!SDL_Vulkan_CreateSurface(m_infos.window->getInternalObject(), m_infos.instance, &m_surface))
 			throw se::exceptions::RuntimeError("Can't create vulkan SDL2 surface : " + std::string(SDL_GetError()));
@@ -22,6 +25,28 @@ namespace se::renderer::vulkan {
 	Surface::~Surface() {
 		if (m_surface != VK_NULL_HANDLE)
 			vkDestroySurfaceKHR(m_infos.instance, m_surface, nullptr);
+	}
+
+
+	void Surface::queryInformations(VkPhysicalDevice physicalDevice) {
+		if (vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, m_surface, &m_surfaceCapabilities) != VK_SUCCESS)
+			throw se::exceptions::RuntimeError("Can't get physical device surface capabilities");
+
+		se::Uint32 count {};
+		if (vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, m_surface, &count, nullptr))
+			throw se::exceptions::RuntimeError("Can't get physiccal device surface formats count");
+
+		m_surfaceFormats.resize(count);
+		if (vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, m_surface, &count, m_surfaceFormats.data()))
+			throw se::exceptions::RuntimeError("Can't get physiccal device surface formats");
+
+
+		if (vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, m_surface, &count, nullptr))
+			throw se::exceptions::RuntimeError("Can't get physiccal device surface present modes count");
+
+		m_surfaceFormats.resize(count);
+		if (vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, m_surface, &count, m_presentModes.data()))
+			throw se::exceptions::RuntimeError("Can't get physiccal device surface present modes");
 	}
 
 
