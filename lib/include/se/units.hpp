7706363d246cc3f3da344a8fe045a6480c71d487
Dv-Senna <dvsenna64@gmail.com>
2024-05-28 11:09:16 +0200

    Make glad a submodule

--- lib/include/se/units.hpp
+++ lib/include/se/units.hpp
@@ -15,18 +15,18 @@ namespace se {
 		static const std::string prefix;
 	};
 
-	template <> const std::string Ratio<1, 1000>::prefix {"m"};
-	template <> const std::string Ratio<1, 1'000'000>::prefix {"μ"};
-	template <> const std::string Ratio<1, 1'000'000'000>::prefix {"n"};
-	template <> const std::string Ratio<1000, 1>::prefix {"k"};
-	template <> const std::string Ratio<1'000'000, 1>::prefix {"M"};
-	template <> const std::string Ratio<1'000'000'000, 1>::prefix {"G"};
-	template <> const std::string Ratio<1024, 1>::prefix {"ki"};
-	template <> const std::string Ratio<1024*1024, 1>::prefix {"Mi"};
-	template <> const std::string Ratio<1024*1024*1024, 1>::prefix {"Gi"};
+	template <> constexpr std::string Ratio<1, 1000>::prefix {"m"};
+	template <> constexpr std::string Ratio<1, 1'000'000>::prefix {"μ"};
+	template <> constexpr std::string Ratio<1, 1'000'000'000>::prefix {"n"};
+	template <> constexpr std::string Ratio<1000, 1>::prefix {"k"};
+	template <> constexpr std::string Ratio<1'000'000, 1>::prefix {"M"};
+	template <> constexpr std::string Ratio<1'000'000'000, 1>::prefix {"G"};
+	template <> constexpr std::string Ratio<1024, 1>::prefix {"ki"};
+	template <> constexpr std::string Ratio<1024*1024, 1>::prefix {"Mi"};
+	template <> constexpr std::string Ratio<1024*1024*1024, 1>::prefix {"Gi"};
 
 	template <se::Uint NUM, se::Uint DEN>
-	const std::string Ratio<NUM, DEN>::prefix {""};
+	constexpr std::string Ratio<NUM, DEN>::prefix {""};
 
 	using Milli = se::Ratio<1, 1000>;
 	using Micro = se::Ratio<1, 1'000'000>;
